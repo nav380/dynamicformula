d81dbae3f7d3be5e7dca0da4e5166134458b06ef
Naveen chauhan <143591216+nav380@users.noreply.github.com>
2025-06-25 21:11:50 +0530

    Update settings.py

--- dyamiccalculater/settings.py
+++ dyamiccalculater/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
 }
 
 
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -117,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
